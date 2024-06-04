7d80c0919683116a6b773c8a6a702c6e62ebe056
sjjns6464994 <153898966+sjjns6464994@users.noreply.github.com>
2024-06-04 15:25:55 +0800

    Add files via upload

---
+++ 1!+2!+3!+...+n!.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int i = 0;
+    int ret = 1;
+    int sum = 0;
+    int j = 0;
+
+    for (j = 1; j <= n; j++)
+    {
+        for (i = 1; i <= j; i++)
+        {
+            ret *= i;
+        }
+        sum += ret;
+    }
+    printf("%d\n", sum);
+    system("pause");
+    return 0;
+}
