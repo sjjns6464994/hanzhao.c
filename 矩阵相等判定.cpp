d4d7da1da1fd207783d705b21d3152bf9dc63e6d
sjjns6464994 <153898966+sjjns6464994@users.noreply.github.com>
2024-07-21 23:07:24 +0800

    Add files via upload

---
+++ 矩阵相等判定.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,m,flag=1;
+    scanf("%d%d",&n,&m);
+    int a[n][m],b[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++)
+     {
+        for(int j=0; j<m; j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(a[i][j] != b[i][j])
+            {
+                flag = 0;
+            }
+        }
+    }
+    if(flag)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
